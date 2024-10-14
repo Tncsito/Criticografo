1f8ed98fc7763b14c99995fbcda693a160eb46f2
tncsito <eduarosantoyo@gmail.com>
2024-10-14 14:52:19 -0700

    .

--- Criticografo_DA_EESA/Criticografo_DA_EESA/MainPage.xaml.cs
+++ Criticografo_DA_EESA/Criticografo_DA_EESA/MainPage.xaml.cs
@@ -14,5 +14,45 @@ namespace Criticografo_DA_EESA
         {
             InitializeComponent();
         }
+
+        private void R_Clicked(object sender, EventArgs e)
+        {
+            string _Nombre = Nombre.Text;            
+            string a = _Nombre + " es";
+            if (M.IsChecked)
+            {
+                a = a + " Mujer, ";
+            }
+            if (H.IsChecked)
+            {
+                a = a + " Hombre, ";
+            }
+            if (Alto.IsChecked)
+            {
+                a = a + " Alto, ";
+            }
+            if (Listo.IsChecked)
+            {
+                a = a + " listo, ";
+            }
+            if (Raro.IsChecked)
+            {
+                a = a + " Raro, ";
+            }
+            if (Feo.IsChecked)
+            {
+                a = a + " Feo, ";
+            }
+            if (Extravagante.IsChecked)
+            {
+                a = a + " Extravagante, ";
+            }
+            if (Grande.IsChecked)
+            {
+                a = a + " Grande. ";
+            }
+            DisplayAlert("Resultado", a, "Cerrar");
+
+        }
     }
 }
